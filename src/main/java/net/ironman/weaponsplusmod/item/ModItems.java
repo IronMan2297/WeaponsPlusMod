40da08af092c8d95a49e815d92bed7045141a08a
IronMan2297 <liamweigel1@icloud.com>
2024-03-13 20:42:13 +0100

    Added ModBlocks and ModItems class

---
+++ src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.ironman.weaponsplusmod.item;
+
+import net.ironman.weaponsplusmod.WeaponsPlusMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, WeaponsPlusMod.MOD_ID);
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
