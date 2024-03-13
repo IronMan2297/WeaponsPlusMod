755c6f2a315ec735b6a6903899f8177a5d3c49a4
IronMan2297 <liamweigel1@icloud.com>
2024-03-13 21:51:28 +0100

    Added Raw Hell Gem and Hell Gem

--- src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
+++ src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
@@ -5,12 +5,16 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, WeaponsPlusMod.MOD_ID);
 
-
+    public static final RegistryObject<Item> RAW_HELL_GEM = ITEMS.register("raw_hell_gem",
+            () -> new Item(new Item.Properties().fireResistant()));
+    public static final RegistryObject<Item> HELL_GEM = ITEMS.register("hell_gem",
+            () -> new Item(new Item.Properties().fireResistant()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
