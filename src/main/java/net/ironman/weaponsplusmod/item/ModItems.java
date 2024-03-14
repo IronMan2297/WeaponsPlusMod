2325d61faeae9aaa8c03b48b9393bacd70440dd0
IronMan2297 <liamweigel1@icloud.com>
2024-03-14 13:47:59 +0100

    changed hell core stackable to 64

--- src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
+++ src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
@@ -20,15 +20,14 @@ public class ModItems {
 
     public static final RegistryObject<Item> PHOENIX_FEATHER = ITEMS.register("phoenix_feather",
             () -> new Item(new Item.Properties()
-                    .stacksTo(4)
+                    .stacksTo(16)
                     .rarity(Rarity.create("ANCIENT", ChatFormatting.GOLD))));
 
     public static final RegistryObject<Item> EMPOWERED_HELL_GEM = ITEMS.register("empowered_hell_gem",
             () -> new Item(new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> HELL_CORE = ITEMS.register("hell_core",
-            () -> new Item(new Item.Properties().fireResistant().stacksTo(1)
-                    .rarity(Rarity.RARE)));
+            () -> new Item(new Item.Properties().fireResistant().rarity(Rarity.RARE)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
