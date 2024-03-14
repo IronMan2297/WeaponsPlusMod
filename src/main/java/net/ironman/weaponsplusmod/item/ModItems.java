eb57186f6bfe4ce264f306bac7c5579126fb06da
IronMan2297 <liamweigel1@icloud.com>
2024-03-14 23:51:44 +0100

    applied fireResitantance on Hell Weapons

--- src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
+++ src/main/java/net/ironman/weaponsplusmod/item/ModItems.java
@@ -31,42 +31,42 @@ public class ModItems {
     /* Standart Hell Weapons and Tools */
     public static final RegistryObject<Item> HELL_SWORD = ITEMS.register("hell_sword",
             () -> new SwordItem(ModToolTiers.HELL, 4,-2.4f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_AXE = ITEMS.register("hell_axe",
             () -> new AxeItem(ModToolTiers.HELL, 6,-3f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_PICKAXE = ITEMS.register("hell_pickaxe",
             () -> new PickaxeItem(ModToolTiers.HELL, 2,-2.8f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_SHOVEL = ITEMS.register("hell_shovel",
             () -> new ShovelItem(ModToolTiers.HELL, 3,-3f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_HOE = ITEMS.register("hell_hoe",
             () -> new HoeItem(ModToolTiers.HELL, -3,0,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
 
     /* Special Hell Weapons */
     public static final RegistryObject<Item> HELL_BATTLE_AXE = ITEMS.register("hell_battle_axe",
             () -> new AxeItem(ModToolTiers.HELL, 7,-3.2f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_DOUBLE_AXE = ITEMS.register("hell_double_axe",
             () -> new AxeItem(ModToolTiers.HELL, 9,-3.4f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_RAPIER = ITEMS.register("hell_rapier",
             () -> new SwordItem(ModToolTiers.HELL, 3,-1.4f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_CUTLASS = ITEMS.register("hell_cutlass",
             () -> new SwordItem(ModToolTiers.HELL, 6,-2.8f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_DAGGER = ITEMS.register("hell_dagger",
             () -> new SwordItem(ModToolTiers.HELL, 3,-0.2f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_KATANA = ITEMS.register("hell_katana",
             () -> new SwordItem(ModToolTiers.HELL, 5,-2.0f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
     public static final RegistryObject<Item> HELL_LONGSWORD = ITEMS.register("hell_longsword",
             () -> new SwordItem(ModToolTiers.HELL, 8,-3.3f,
-                    new Item.Properties().durability(256)));
+                    new Item.Properties().durability(256).fireResistant()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
