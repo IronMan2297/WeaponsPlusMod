42c56a6693c1e93081b7e3fba9a3e49c6559f26f
IronMan2297 <liamweigel1@icloud.com>
2024-05-03 20:23:42 +0200

    little change

--- src/main/java/net/ironman/weaponsplusmod/event/ModEvents.java
+++ src/main/java/net/ironman/weaponsplusmod/event/ModEvents.java
@@ -1,16 +1,25 @@
 package net.ironman.weaponsplusmod.event;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.ironman.weaponsplusmod.WeaponsPlusMod;
+import net.ironman.weaponsplusmod.item.ModItems;
 import net.ironman.weaponsplusmod.item.custom.HammerItem;
+import net.ironman.weaponsplusmod.villager.ModVillagers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = WeaponsPlusMod.MOD_ID)
@@ -39,5 +48,43 @@ public class ModEvents {
                 HARVESTED_BLOCKS.remove(pos);
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == ModVillagers.BLACKSMITH.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 12),
+                    new ItemStack(ModItems.HAMMER.get()), 2, 4, 0.02f
+            ));
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    new ItemStack(ModItems.IRON_ROD.get()), 7, 2, 0.06f
+            ));
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.HELL_GEM.get(), 6),
+                    new ItemStack(Items.EMERALD, 2), 7, 2, 0.04f
+            ));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 18),
+                    new ItemStack(ModItems.IRON_MULTITOOL.get()), 2, 4, 0.02f
+            ));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(ModItems.SHARD_OF_THE_OCEAN.get(), 3),
+                    new ItemStack(Items.DIAMOND), 2, 4, 0.04f
+            ));
+        }
+    }
+    @SubscribeEvent
+    public static void addWandererCustomTrades(WandererTradesEvent event) {
+            List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+            List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
+
+            genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 15),
+                    new ItemStack(ModItems.PHOENIX_FEATHER.get(),1), 2, 4, 0.02f
+            ));
     }
 }
